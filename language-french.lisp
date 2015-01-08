a291283a1f9b4ab1a2d71c1c2868f0a992df6111
Robert Strandh <robert.strandh@gmail.com>
2015-01-08 18:43:11 +0100

    Renamed class for French language to use English name of langauge.

--- language-french.lisp
+++ language-french.lisp
@@ -1,54 +1,54 @@
 (cl:in-package #:acclimation)
 
-(defclass francais (language)
+(defclass french (language)
   ())
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Methods on LONG-DAY-NAME.
 
-(defmethod long-day-name ((day (eql 1)) (language francais))
+(defmethod long-day-name ((day (eql 1)) (language french))
   "lundi")
 
-(defmethod long-day-name ((day (eql 2)) (language francais))
+(defmethod long-day-name ((day (eql 2)) (language french))
   "mardi")
 
-(defmethod long-day-name ((day (eql 3)) (language francais))
+(defmethod long-day-name ((day (eql 3)) (language french))
   "mercredi")
 
-(defmethod long-day-name ((day (eql 4)) (language francais))
+(defmethod long-day-name ((day (eql 4)) (language french))
   "jeudi")
 
-(defmethod long-day-name ((day (eql 5)) (language francais))
+(defmethod long-day-name ((day (eql 5)) (language french))
   "vendredi")
 
-(defmethod long-day-name ((day (eql 6)) (language francais))
+(defmethod long-day-name ((day (eql 6)) (language french))
   "samedi")
 
-(defmethod long-day-name ((day (eql 7)) (language francais))
+(defmethod long-day-name ((day (eql 7)) (language french))
   "dimanche")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Methods on SHORT-DAY-NAME.
 
-(defmethod short-day-name ((day (eql 1)) (language francais))
+(defmethod short-day-name ((day (eql 1)) (language french))
   "lun")
 
-(defmethod short-day-name ((day (eql 2)) (language francais))
+(defmethod short-day-name ((day (eql 2)) (language french))
   "mar")
 
-(defmethod short-day-name ((day (eql 3)) (language francais))
+(defmethod short-day-name ((day (eql 3)) (language french))
   "mer")
 
-(defmethod short-day-name ((day (eql 4)) (language francais))
+(defmethod short-day-name ((day (eql 4)) (language french))
   "jeu")
 
-(defmethod short-day-name ((day (eql 5)) (language francais))
+(defmethod short-day-name ((day (eql 5)) (language french))
   "ven")
 
-(defmethod short-day-name ((day (eql 6)) (language francais))
+(defmethod short-day-name ((day (eql 6)) (language french))
   "sam")
 
-(defmethod short-day-name ((day (eql 7)) (language francais))
+(defmethod short-day-name ((day (eql 7)) (language french))
   "dim")
