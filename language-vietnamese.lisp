bc2ed179832135852c420f769f4f5f876772da97
Robert Strandh <robert.strandh@gmail.com>
2015-11-18 17:52:10 +0100

    Added methods on long-day-name for language Vietnamese.

--- language-vietnamese.lisp
+++ language-vietnamese.lisp
@@ -2,3 +2,28 @@
 
 (defclass vietnamese (language)
   ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on LONG-DAY-NAME.
+
+(defmethod long-day-name ((day (eql 1)) (language vietnamese))
+  "ngày thứ hai")
+
+(defmethod long-day-name ((day (eql 2)) (language vietnamese))
+  "ngày thứ ba")
+
+(defmethod long-day-name ((day (eql 3)) (language vietnamese))
+  "ngày thứ tư")
+
+(defmethod long-day-name ((day (eql 4)) (language vietnamese))
+  "ngày thứ năm")
+
+(defmethod long-day-name ((day (eql 5)) (language vietnamese))
+  "ngày thứ sáu ")
+
+(defmethod long-day-name ((day (eql 6)) (language vietnamese))
+  "ngày thứ bảy")
+
+(defmethod long-day-name ((day (eql 7)) (language vietnamese))
+  "chủ nhật")
