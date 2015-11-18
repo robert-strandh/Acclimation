08790cadb9a47858fd9e9690198ea87e79ba83f3
Robert Strandh <robert.strandh@gmail.com>
2015-11-18 17:53:16 +0100

    Added methods on short-day-name for language Vietnamese.

--- language-vietnamese.lisp
+++ language-vietnamese.lisp
@@ -27,3 +27,28 @@
 
 (defmethod long-day-name ((day (eql 7)) (language vietnamese))
   "chủ nhật")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on SHORT-DAY-NAME.
+
+(defmethod short-day-name ((day (eql 1)) (language vietnamese))
+  "thứ hai")
+
+(defmethod short-day-name ((day (eql 2)) (language vietnamese))
+  "thứ ba")
+
+(defmethod short-day-name ((day (eql 3)) (language vietnamese))
+  "thứ tư")
+
+(defmethod short-day-name ((day (eql 4)) (language vietnamese))
+  "thứ năm")
+
+(defmethod short-day-name ((day (eql 5)) (language vietnamese))
+  "thứ sáu ")
+
+(defmethod short-day-name ((day (eql 6)) (language vietnamese))
+  "thứ bảy")
+
+(defmethod short-day-name ((day (eql 7)) (language vietnamese))
+  "chủ nhật")
