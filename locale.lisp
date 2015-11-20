e790cbbaecaeb6ae9929cff707ad37df6b9d63f5
Robert Strandh <robert.strandh@gmail.com>
2015-11-20 08:58:50 +0100

    Add slot WEEK-START to class LOCALE.

--- locale.lisp
+++ locale.lisp
@@ -1,6 +1,13 @@
 (cl:in-package #:acclimation)
 
 (defclass locale ()
-  ((%language :initarg :language :accessor language)))
+  ((%language :initarg :language :accessor language)
+   ;; This slot holds an integer between 1 and 7 which is the ISO 8601
+   ;; day number of the day that is to be considered the first day of
+   ;; the week.  By default, we consider Monday to be the first one,
+   ;; as ISO 8601 stipulates.
+   (%week-start :initarg :week-start
+		:initform 1
+		:accessor week-start)))
 
 (defvar *locale*)
