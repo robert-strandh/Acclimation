236662a939c8f59563fc74a3b7619ad286827fe3
Robert Strandh <robert.strandh@gmail.com>
2015-11-26 13:10:05 +0100

    Method on FORMAT-AMBIENT-TEMPERATURE specialized to AMBIENT-TEMPOERATURE-CELSIUS.

--- Ambient-temperature/ambient-temperature.lisp
+++ Ambient-temperature/ambient-temperature.lisp
@@ -11,3 +11,16 @@
   ())
 
 (defgeneric format-ambient-temperature (absolute-temperature formater stream))
+
+(defmethod format-ambient-temperature
+    (absolute-tempoerature
+     (formater ambient-temperature-celsius)
+     stream-designator)
+  (if (zerop (decimal-count formater))
+      (format stream-designator
+	      "~d°C"
+	      (round (- absolute-tempoerature 273.15d0)))
+      (format stream-designator
+	      "~,vf°C"
+	      (decimal-count formater)
+	      (- absolute-tempoerature 273.15d0))))
