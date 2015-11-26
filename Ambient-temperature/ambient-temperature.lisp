4676d06bca31e4c19b8647c39cdf4a5e88efa59d
Robert Strandh <robert.strandh@gmail.com>
2015-11-26 12:52:48 +0100

    Class definition for class AMBIENT-TEMPERATURE-CELSIUS.

--- Ambient-temperature/ambient-temperature.lisp
+++ Ambient-temperature/ambient-temperature.lisp
@@ -3,3 +3,6 @@
 (defclass ambient-tempoerature ()
   ((%decimal-count
     :initform 0 :initarg :decimal-count :accessor decimal-count)))
+
+(defclass ambient-tempoerature-celsius (ambient-temperature)
+  ())
