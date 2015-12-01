98db88952b94f8b6bf9cdbb96ddfa1d414d90223
Robert Strandh <robert.strandh@gmail.com>
2015-12-01 07:48:11 +0100

    New function for converting Celsius to Kelvin.

--- Temperature/temperature.lisp
+++ Temperature/temperature.lisp
@@ -3,6 +3,9 @@
 (defun kelvin-to-celsius (kelvin)
   (- kelvin 273.15d0))
 
+(defun celsius-to-kelvin (celsius)
+  (+ celsius 273.15d0))
+
 (defun kelvin-to-fahrenheit (kelvin)
   (let ((celsius (kelvin-to-celsius kelvin)))
     (+ (* 9/5 celsius) 32)))
