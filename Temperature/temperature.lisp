ef7e3467e95f14460f04ae147e8588f8e6329119
Robert Strandh <robert.strandh@gmail.com>
2015-12-01 07:49:44 +0100

    New function for converting Fahrenheit to Kelvin.

--- Temperature/temperature.lisp
+++ Temperature/temperature.lisp
@@ -10,3 +10,6 @@
   (let ((celsius (kelvin-to-celsius kelvin)))
     (+ (* 9/5 celsius) 32)))
 
+(defun fahrenheit-to-kelvin (fahrenheit)
+  (let ((celsius (* 5/9 (- fahrenheit 32))))
+    (celsius-to-kelvin celsius)))
