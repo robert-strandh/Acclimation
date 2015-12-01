b43560a1ceef3b7a9e8d83d08e2c03a2e2a132b4
Robert Strandh <robert.strandh@gmail.com>
2015-12-01 07:55:27 +0100

    Fix another typo.

--- Temperature/oven-temperature.lisp
+++ Temperature/oven-temperature.lisp
@@ -1,29 +1,29 @@
 (cl:in-package #:acclimation-temperature)
 
-(defclass ovent-temperature ()
+(defclass oven-temperature ()
   ((%decimal-count
     :initform 0 :initarg :decimal-count :accessor decimal-count)))
 
-(defclass ovent-temperature-celsius (ovent-temperature)
+(defclass oven-temperature-celsius (oven-temperature)
   ())
 
-(defclass ovent-temperature-fahrenheit (ovent-temperature)
+(defclass oven-temperature-fahrenheit (oven-temperature)
   ())
 
-(defgeneric format-ovent-temperature (absolute-temperature formater stream))
+(defgeneric format-oven-temperature (absolute-temperature formater stream))
 
-(defmethod format-ovent-temperature
+(defmethod format-oven-temperature
     (absolute-temperature
-     (formater ovent-temperature-celsius)
+     (formater oven-temperature-celsius)
      stream-designator)
   (let ((celsius-temperature (kelvin-to-celsius absolute-temperature)))
     (format stream-designator
 	    "~d°C"
 	    (* 5 (round celsius-temperature 5)))))
 
-(defmethod format-ovent-temperature
+(defmethod format-oven-temperature
     (absolute-temperature
-     (formater ovent-temperature-fahrenheit)
+     (formater oven-temperature-fahrenheit)
      stream-designator)
   (let ((fahrenheit-temperature (kelvin-to-fahrenheit absolute-temperature)))
     (format stream-designator
