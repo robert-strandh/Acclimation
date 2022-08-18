32ca9a323a04347e898ad8b7c712106538747dcb
Robert Strandh <robert.strandh@gmail.com>
2022-08-18 14:06:42 +0200

    Define SWEDISH methods on LONG-MONTH-NAME and SHORT-MONTH-NAME.

--- language-swedish.lisp
+++ language-swedish.lisp
@@ -52,3 +52,83 @@
 
 (defmethod short-day-name ((day (eql 7)) (language swedish))
   "sön")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on LONG-MONTH-NAME.
+
+(defmethod long-month-name ((month (eql 1)) (langauge swedish))
+  "januari")
+
+(defmethod long-month-name ((month (eql 2)) (langauge swedish))
+  "februari")
+
+(defmethod long-month-name ((month (eql 3)) (langauge swedish))
+  "mars")
+
+(defmethod long-month-name ((month (eql 4)) (langauge swedish))
+  "april")
+
+(defmethod long-month-name ((month (eql 5)) (langauge swedish))
+  "maj")
+
+(defmethod long-month-name ((month (eql 6)) (langauge swedish))
+  "juni")
+
+(defmethod long-month-name ((month (eql 7)) (langauge swedish))
+  "juli")
+
+(defmethod long-month-name ((month (eql 8)) (langauge swedish))
+  "augusti")
+
+(defmethod long-month-name ((month (eql 9)) (langauge swedish))
+  "september")
+
+(defmethod long-month-name ((month (eql 10)) (langauge swedish))
+  "oktober")
+
+(defmethod long-month-name ((month (eql 11)) (langauge swedish))
+  "november")
+
+(defmethod long-month-name ((month (eql 12)) (langauge swedish))
+  "december")
+                            
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on SHORT-MONTH-NAME.
+
+(defmethod short-month-name ((month (eql 1)) (langauge swedish))
+  "jan")
+
+(defmethod short-month-name ((month (eql 2)) (langauge swedish))
+  "feb")
+
+(defmethod short-month-name ((month (eql 3)) (langauge swedish))
+  "mar")
+
+(defmethod short-month-name ((month (eql 4)) (langauge swedish))
+  "apr")
+
+(defmethod short-month-name ((month (eql 5)) (langauge swedish))
+  "maj")
+
+(defmethod short-month-name ((month (eql 6)) (langauge swedish))
+  "jun")
+
+(defmethod short-month-name ((month (eql 7)) (langauge swedish))
+  "jul")
+
+(defmethod short-month-name ((month (eql 8)) (langauge swedish))
+  "aug")
+
+(defmethod short-month-name ((month (eql 9)) (langauge swedish))
+  "sep")
+
+(defmethod short-month-name ((month (eql 10)) (langauge swedish))
+  "okt")
+
+(defmethod short-month-name ((month (eql 11)) (langauge swedish))
+  "nov")
+
+(defmethod short-month-name ((month (eql 12)) (langauge swedish))
+  "dec")
