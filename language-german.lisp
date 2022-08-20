13e8649300b0c436ca22da484f4b6b8334bf251b
Robert Strandh <robert.strandh@gmail.com>
2022-08-20 08:25:09 +0200

    Define GERMAN methods on LONG-MONTH-NAME and SHORT-MONTH-NAME.

--- language-german.lisp
+++ language-german.lisp
@@ -52,3 +52,83 @@
 
 (defmethod short-day-name ((day (eql 7)) (language german))
   "Son")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on LONG-MONTH-NAME.
+
+(defmethod long-month-name ((month (eql 1)) (langauge german))
+  "Januar")
+
+(defmethod long-month-name ((month (eql 2)) (langauge german))
+  "Februar")
+
+(defmethod long-month-name ((month (eql 3)) (langauge german))
+  "März")
+
+(defmethod long-month-name ((month (eql 4)) (langauge german))
+  "April")
+
+(defmethod long-month-name ((month (eql 5)) (langauge german))
+  "Mai")
+
+(defmethod long-month-name ((month (eql 6)) (langauge german))
+  "Juni")
+
+(defmethod long-month-name ((month (eql 7)) (langauge german))
+  "Juli")
+
+(defmethod long-month-name ((month (eql 8)) (langauge german))
+  "August")
+
+(defmethod long-month-name ((month (eql 9)) (langauge german))
+  "September")
+
+(defmethod long-month-name ((month (eql 10)) (langauge german))
+  "Oktober")
+
+(defmethod long-month-name ((month (eql 11)) (langauge german))
+  "November")
+
+(defmethod long-month-name ((month (eql 12)) (langauge german))
+  "Dezember")
+                            
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Methods on SHORT-MONTH-NAME.
+
+(defmethod short-month-name ((month (eql 1)) (langauge german))
+  "Jan")
+
+(defmethod short-month-name ((month (eql 2)) (langauge german))
+  "Feb")
+
+(defmethod short-month-name ((month (eql 3)) (langauge german))
+  "Mär")
+
+(defmethod short-month-name ((month (eql 4)) (langauge german))
+  "Apr")
+
+(defmethod short-month-name ((month (eql 5)) (langauge german))
+  "Mai")
+
+(defmethod short-month-name ((month (eql 6)) (langauge german))
+  "Jun")
+
+(defmethod short-month-name ((month (eql 7)) (langauge german))
+  "Jul")
+
+(defmethod short-month-name ((month (eql 8)) (langauge german))
+  "Aug")
+
+(defmethod short-month-name ((month (eql 9)) (langauge german))
+  "Sep")
+
+(defmethod short-month-name ((month (eql 10)) (langauge german))
+  "Okt")
+
+(defmethod short-month-name ((month (eql 11)) (langauge german))
+  "Nov")
+
+(defmethod short-month-name ((month (eql 12)) (langauge german))
+  "Dez")
